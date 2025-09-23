c9115361d10e94bcfb9a4fc56e1a99a223cb1c14
Your Name <nunesikaro@gmail.com>
2025-09-23 17:50:48 -0300

    add if elif else

---
+++ primeiros codigos/if elif else/ifelifelse.py
@@ -0,0 +1,14 @@
+idade = 18
+
+if idade < 18:
+    print("Criança")
+elif idade >= 18 and idade < 60:
+    print("Adulto")
+elif idade >= 12 and idade < 18:
+    print("Adolescente")
+elif idade == 60 :
+    print("Idoso")
+else:
+    print("Terceira idade")
+
+# Estruturas Condicionais em Python
---
+++ primeiros codigos/if elif else/notas.py
@@ -0,0 +1,15 @@
+nota = 90
+
+if nota >= 90:
+    print("Parabéns! Você recebeu um A e Passou!")
+elif nota >= 80:
+    print("Muito bem! Você recebeu um B.")
+elif nota >= 70:
+    print("Bom trabalho! Você recebeu um C.")
+elif nota >= 60:
+    print("Você recebeu um D. Precisa melhorar.")
+elif nota >= 50:
+    print("Infelizmente você reprovou!")
+else:
+    print("Nota inválida. Por favor, insira uma nota entre 0 e 100.")
+# Estruturas Condicionais em Python
