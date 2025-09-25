d28494ccb76983dfd3f7a90b1c24054d96555a62
Your Name <nunesikaro@gmail.com>
2025-09-25 03:53:11 -0300

    loops adicionado

---
+++ primeiros codigos/loops/os loops/exmpl.py
@@ -0,0 +1,11 @@
+print("Numeros de 1 a 5 multiplicados por 2:")
+for numero in range(1, 6):  # range(1, 6) gera números de 1 a 5
+    print(numero * 2)
+
+
+print("Numeros de 1 a 5 muiltiplicados por 2 usando while:")
+contador = 1
+while contador <= 5:
+    print(contador * 2)
+    contador += 1
+
---
+++ primeiros codigos/loops/os loops/loop_FOR.py
@@ -0,0 +1,16 @@
+#O loop for é utilizado para iterar sobre uma sequência (como uma lista, uma tupla ou uma string) ou qualquer objeto iterável. A sintaxe básica é a seguinte:
+
+"""for variável in sequência:
+
+     Bloco de código a repetir
+    instruções"""
+
+#Exemplo 1: Iterando sobre uma lista
+frutas = ["maçã", "banana", "cereja"]
+
+for fruta in frutas:
+    print(fruta)
+#Saída:
+#maçã
+#banana
+#cereja
